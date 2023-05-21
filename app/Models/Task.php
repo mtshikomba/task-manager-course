682f9960a2b7e88393b56bf134c39325eeec9203
Matheus Shikomba <tangenishikomba@gmail.com>
2023-05-20 20:07:08 -0400

    User and Task Models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'description', 'completed'
+    ];
+
+    /**
+     * A task can belong to one User only
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
