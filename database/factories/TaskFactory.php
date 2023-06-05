ff116f7ff8acfbdb161ce4586638f4a59094bb70
Matheus Shikomba <tangenishikomba@gmail.com>
2023-06-04 22:10:05 -0400

    Added views, seeder and factory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,10 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'completed' => $this->faker->boolean,
+            'user_id' => User::first()->id
         ];
     }
 }
